26ff1d8da7f1b9ff1739a320f2486886ce951d55
jin1125 <shogo.jinta@gmail.com>
2021-09-11 00:01:44 +0900

    fix homeAddress

--- pages/mypage/edit.js
+++ pages/mypage/edit.js
@@ -180,7 +180,7 @@ export default function edit() {
 
     const profileInformation = {
       profileImageUrl: profileUrl,
-      userName:userName,
+      userName: userName,
       freeImageUrl0: freeUrl0,
       freeImageUrl1: freeUrl1,
       freeImageUrl2: freeUrl2,
@@ -206,7 +206,7 @@ export default function edit() {
 
     /// 名前変更の処理 ///
     auth.onAuthStateChanged((user) => {
-      if(user){
+      if (user) {
         user.updateProfile({
           displayName: userName,
         });
@@ -392,7 +392,7 @@ export default function edit() {
               user
                 .delete()
                 .then(() => {
-                  setUserName('')
+                  setUserName("");
                   alert.success("アカウントを削除しました");
                   Router.push("/login");
                 })
@@ -435,7 +435,7 @@ export default function edit() {
           user
             .delete()
             .then(() => {
-              setUserName('')
+              setUserName("");
               alert.success("アカウントを削除しました");
               Router.push("/login");
             })
@@ -699,17 +699,65 @@ export default function edit() {
               <div className="flex flex-row flex-wrap gap-1 items-center">
                 <Emoji emoji="round_pushpin" size={20} />
                 <label>
-                  <input
-                    type="text"
-                    value={homeAddress}
-                    onChange={(e) => setHomeAddress(e.target.value.trim())}
-                    placeholder="住所(〜市区郡)"
+                  <select
+                    className="bg-blue-100 rounded-full outline-none pl-3 py-1"
                     name="homeAddress"
-                    maxLength="10"
-                    className="text-base bg-blue-100 placeholder-blue-300 text-center rounded-full py-1 outline-none"
-                  />
+                    value={homeAddress}
+                    onChange={(e) => setDobMM(e.target.value)}
+                    onChange={(e) => setHomeAddress(e.target.value)}
+                  >
+                    <option value="">住所</option>
+                    <option value="北海道">北海道</option>
+                    <option value="青森県">青森県</option>
+                    <option value="岩手県">岩手県</option>
+                    <option value="宮城県">宮城県</option>
+                    <option value="秋田県">秋田県</option>
+                    <option value="山形県">山形県</option>
+                    <option value="福島県">福島県</option>
+                    <option value="茨城県">茨城県</option>
+                    <option value="栃木県">栃木県</option>
+                    <option value="群馬県">群馬県</option>
+                    <option value="埼玉県">埼玉県</option>
+                    <option value="千葉県">千葉県</option>
+                    <option value="東京都">東京都</option>
+                    <option value="神奈川県">神奈川県</option>
+                    <option value="新潟県">新潟県</option>
+                    <option value="富山県">富山県</option>
+                    <option value="石川県">石川県</option>
+                    <option value="福井県">福井県</option>
+                    <option value="山梨県">山梨県</option>
+                    <option value="長野県">長野県</option>
+                    <option value="岐阜県">岐阜県</option>
+                    <option value="静岡県">静岡県</option>
+                    <option value="愛知県">愛知県</option>
+                    <option value="三重県">三重県</option>
+                    <option value="滋賀県">滋賀県</option>
+                    <option value="京都府">京都府</option>
+                    <option value="大阪府">大阪府</option>
+                    <option value="兵庫県">兵庫県</option>
+                    <option value="奈良県">奈良県</option>
+                    <option value="和歌山県">和歌山県</option>
+                    <option value="鳥取県">鳥取県</option>
+                    <option value="島根県">島根県</option>
+                    <option value="岡山県">岡山県</option>
+                    <option value="広島県">広島県</option>
+                    <option value="山口県">山口県</option>
+                    <option value="徳島県">徳島県</option>
+                    <option value="香川県">香川県</option>
+                    <option value="愛媛県">愛媛県</option>
+                    <option value="高知県">高知県</option>
+                    <option value="福岡県">福岡県</option>
+                    <option value="佐賀県">佐賀県</option>
+                    <option value="長崎県">長崎県</option>
+                    <option value="熊本県">熊本県</option>
+                    <option value="大分県">大分県</option>
+                    <option value="宮崎県">宮崎県</option>
+                    <option value="鹿児島県">鹿児島県</option>
+                    <option value="沖縄県">沖縄県</option>
+                  </select>
                   <span className="text-red-500 align-top">*</span>
                 </label>
+
               </div>
 
               <div className="flex flex-row flex-wrap gap-1 items-center">
@@ -1172,8 +1220,8 @@ export default function edit() {
                 )}
                 {isGoogleLogin && (
                   <p className="w-11/12 my-3 text-center mx-auto text-blue-300 text-sm">
-                  google認識でログインしたアカウントはメールアドレスの変更ができません
-                </p>
+                    google認識でログインしたアカウントはメールアドレスの変更ができません
+                  </p>
                 )}
               </>
             )}
