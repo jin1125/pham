9bcc89a7f9ee5ebdaa3263ed7b7552a3056ca5fe
jin1125 <shogo.jinta@gmail.com>
2021-09-30 17:09:51 +0900

    add head data

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,8 +14,16 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="ja">
+      <Html lang="ja" prefix="og: http://ogp.me/ns#">
         <Head>
+          <meta name="description" content="薬局薬剤師さんのお仕事SNSです。" />
+          <meta property="og:url" content="https://pham.vercel.app/" />
+          <meta property="og:title" content="Pham" />
+          <meta property="og:type" content="website" />
+          <meta property="og:description" content="薬局薬剤師さんのお仕事SNSです。" />
+          <meta property="og:image" content="/ogp.png" />
+          <meta property="og:site_name" content="Pham" />
+          <meta property="og:locale" content="ja_JP" />
           <link
             href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap"
             rel="stylesheet"
