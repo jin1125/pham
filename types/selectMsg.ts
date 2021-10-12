a7c721cd9653f2b2f93c5298351fdcc798702541
jin1125 <shogo.jinta@gmail.com>
2021-10-12 14:45:46 +0900

    fix selectMsg.ts

---
+++ types/selectMsg.ts
@@ -0,0 +1,45 @@
+export type SelectMsg = {
+  birthPlace?: string;
+  certification?: string;
+  dobDD?: string;
+  dobMM?: string;
+  dobYY?: string;
+  dream?: string;
+  experiences?: {
+    experience: string;
+    years: string;
+  }[];
+  freeImageUrl0?: string;
+  freeImageUrl1?: string;
+  freeImageUrl2?: string;
+  hobby?: string;
+  homeAddress?: string;
+  jobTitle?: string;
+  language?: string;
+  profileImageUrl?: string;
+  resumes?: {
+    companyName: string;
+    employmentStatus: string;
+    workEnd: string;
+    workStart: string;
+  }[];
+  school?: string;
+  scout?: string;
+  strongArea?: string;
+  subjectArea?: string;
+  userName?: string;
+  comments?: string;
+  companyAddress?: string;
+  companyImageUrl?: string;
+  companyName?:string;
+  companyPrefecture?: string;
+  establishment?: string;
+  exchange?: string;
+  lastmodified?: number;
+  objectID?: string;
+  pharmacies?: string;
+  presidentHobby?: string;
+  unique?: string;
+  websiteUrl?: string;
+__position?: number;
+  };
